4b8f234b964349edc41ce0230386d953d2c16010
betageriashwini13-dev <betageriashwini13@gmail.com>
2026-09-18 15:31:51 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+addition = a + b
+substraction = a - b
+
+print("Addition =", addition)
+print("Substraction =", substraction)
